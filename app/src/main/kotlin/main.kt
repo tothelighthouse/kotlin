d9b15653741ab040b44b1e91ab22996d9536e3d6
Administrator <dddd>
2021-07-24 11:51:26 +0900

    lijlkj

--- app/src/main/kotlin/main.kt
+++ app/src/main/kotlin/main.kt
@@ -1,5 +1,10 @@
+import org.w3c.dom.CanvasRenderingContext2D
+import org.w3c.dom.HTMLCanvasElement
+import org.w3c.dom.HTMLElement
 import org.w3c.dom.HTMLInputElement
+import org.w3c.dom.css.CSSStyleDeclaration
 import org.w3c.dom.events.KeyboardEvent
+import org.w3c.fetch.RequestInit
 import kotlin.browser.document
 
 fun main() {
@@ -92,7 +97,7 @@ val request = RequestBuilder("http://apiServer"){
 enum class Method{POST, GET}
 class Request(
         val url:String,
-        val method:Method,
+        val method: RequestInit,
         val form:MutableMap<String, String>?,
         val timeout:Int,
         val ok:listener?,
@@ -111,3 +116,216 @@ class RequestBuilder(private val url:String){
     var ok:listener? = null
     var fail:listener? = null
 }
+
+// infix method
+// 1
+class ListA{
+    val list = mutableListOf<String>()
+    operator fun get(i:Int) = list[i]
+    operator fun plus(b:String) = run {
+        list += b
+        this
+    }
+    infix fun add(b:String) = plus(b)
+}
+//val list = ListA() + "abc"
+//println( list[0] )
+//println( (list add "def")[1] )
+
+// 2
+infix fun <T> T.combine(v:T) = mutableListOf(this, v)
+infix fun <T> MutableList<T>.combine(v:T) = run{
+    this.add(v)
+    this
+}
+val list = 10 combine 20 combine 30 combine 40
+//println("${JSON.stringify(list)}")
+
+// 3
+public infix fun <A, B> A.to(that: B): Pair<A, B> = Pair(this, that)
+val map = mapOf("a" to 1, "b" to 2)
+
+//constructor
+
+// 1
+open public class ClassTest0{
+    private val propA:String
+    private val propB:String
+    public constructor(a:String, b:String){
+        println("constructor1")
+        propA = a
+        propB = b
+    }
+    public constructor(a:String):this(a, "b"){
+        println("constructor2")
+    }
+}
+
+// 2
+class ClassTest0{
+    private val propA:String
+    private val propB:String
+    constructor(a:String, b:String){
+        println("constructor1")
+        propA = a
+        propB = b
+    }
+    constructor(a:String):this(a, "b"){
+        println("constructor2")
+    }
+}
+// 3
+class ClassTest0 constructor(a:String, b:String){
+    private val propA:String
+    private val propB:String
+    init{
+        println("constructor1")
+        propA = a
+        propB = b
+    }
+    constructor(a:String):this(a, "b"){
+        println("constructor2")
+    }
+}
+// 4
+ class ClassTest0(a:String, b:String){
+    private val propA:String
+    private val propB:String
+    init{
+    println("constructor1")
+    propA = a
+    propB = b
+    }
+    constructor(a:String):this(a, "b"){
+    println("constructor2")
+    }
+}
+
+// 5
+class ClassTest0(private val propA:String, private val propB:String){
+    init{
+        println("constructor1")
+    }
+    constructor(a:String):this(a, "b"){
+        println("constructor2")
+    }
+}
+// 6
+class ClassTest0(private val propA:String, private val propB:String){
+    constructor(a:String):this(a, "b")
+}
+
+// 7
+//open class ClassTest0(private val propA:String, private val propB:String){
+//    constructor(a:String):this(a, "b")
+//}
+//class ClassTest1:ClassTest0("a", "b"){
+//    private val propC = "c"
+//}
+//class ClassTest1(a:String, b:String, c:String):ClassTest0(a, b){
+//    private val propC = c
+//}
+//class ClassTest1(a:String, b:String, private val propC:String):ClassTest0(a, b)
+
+//operator overloading
+//simple map
+class Map{
+    private val map = mutableMapOf<String, String>()
+    operator fun get(key:String) = map[key]
+    operator fun set(key:String, value:String){map[key] = value}
+}
+//val m = Map()
+////m["test"] = "123"
+////println(m["test"])
+
+class Map{
+    private val map = mutableMapOf<String, String>()
+    operator fun get(key:String) = map[key]
+    operator fun set(key:String, value:String){map[key] = value}
+    val name:String? get() = map["name"]
+    var job:String? get() = map["job"]
+        set(value){value?.let{map["job"] = it}}
+}
+//val m = Map()
+//m["name"] = "hika"
+//println(m.name)
+//m.job = "developer"
+//println(m.job)
+
+// by, by lazy
+class Map{
+    private val map = mutableMapOf<String, String>()
+    operator fun get(key:String) = map[key]
+    operator fun set(key:String, value:String){map[key] = value}
+    val name by lazy{map["firstName"] + " " + map["lastName"]}
+}
+
+//html builder
+abstract class El(val tagName:String){
+    protected val el = when(tagName){
+        "body" -> document.body ?: throw Throwable("no body")
+        else -> document.createElement(tagName) as HTMLElement
+    }
+    var html:String get() = el.innerHTML
+        set(value){el.innerHTML = value}
+    operator fun get(key:String) = el.getAttribute(key) ?: ""
+    operator fun set(key:String, value: Any) = el.setAttribute(key, "$value")
+    operator fun invoke() = el
+    operator fun plusAssign(child:El){el.appendChild(child.el)}
+    operator fun minusAssign(child:El){el.removeChild(child.el)}
+    val style: CSSStyleDeclaration get() = el.style
+}
+
+object Body:El("body")
+class Div:El("div")
+class Canvas:El("canvas"){
+    val context: CanvasRenderingContext2D? get() =
+        (el as? HTMLCanvasElement)?.getContext("2d") as? CanvasRenderingContext2D
+}
+
+fun htmlBuilder(){
+    (0..5).map{Div().apply{html = "div-$it"}}.forEach {Body += it}
+    Body += Canvas().apply {
+        this["width"] = 500
+        this["height"] = 500
+        context?.run {
+            lineWidth = 10.0
+            strokeRect(75.0, 140.0, 150.0, 110.0)
+            fillRect(130.0, 190.0, 40.0, 60.0)
+            moveTo(50.0, 140.0)
+            lineTo(150.0, 60.0)
+            lineTo(250.0, 140.0)
+            closePath()
+            stroke()
+        }
+    }
+}
+
+////fetch builder
+//
+//class FetchParam{
+//    val queries = mutableMapOf<String, Any>()
+//    val headers = mutableMapOf<String, String>()
+//    var method = "GET"
+//}
+//fun fetch(url:String, block:FetchParam.()->Unit)= FetchParam().apply{block()}.let{
+//    window.fetch(Request(url, RequestInit(
+//            method = it.method,
+//            headers = run{
+//                val obj = js("{}")
+//                it.headers.forEach {(k, v)->obj[k] = v}
+//                obj
+//            },
+//            body = if(it.method != "GET") it.queries.toList().joinToString("&"){
+//                (k, v)->"$k=$v"
+//            }
+//            else null
+//    )))
+//}
+//fun testFetch() {
+//    fetch("test.json") {}.then {
+//        it.text()
+//    }.then {
+//        println(it)
+//    }
+//}
---
+++ app/src/main/kotlin/main2.kt
@@ -0,0 +1,31 @@
+import org.w3c.fetch.RequestInit
+import kotlin.browser.window
+
+//fetch builder
+
+class FetchParam{
+    val queries = mutableMapOf<String, Any>()
+    val headers = mutableMapOf<String, String>()
+    var method = "GET"
+}
+fun fetch(url:String, block:FetchParam.()->Unit)= FetchParam().apply{block()}.let{
+    window.fetch(Request(url, RequestInit(
+            method = it.method,
+            headers = run{
+                val obj = js("{}")
+                it.headers.forEach {(k, v)->obj[k] = v}
+                obj
+            },
+            body = if(it.method != "GET") it.queries.toList().joinToString("&"){
+                (k, v)->"$k=$v"
+            }
+            else null
+    )))
+}
+fun testFetch() {
+    fetch("test.json") {}.then {
+        it.text()
+    }.then {
+        println(it)
+    }
+}
